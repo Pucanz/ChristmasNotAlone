2d0d779d9ccdc04e3f624284f86a3c9537399882
Tiago Elvas <tfselvas@gmail.com>
2024-12-18 18:28:39 +0000

    tables fetch the data from db. We need to format everything

--- finalProject/src/main/java/org/codeforall/controllers/rest/RestFamilyController.java
+++ finalProject/src/main/java/org/codeforall/controllers/rest/RestFamilyController.java
@@ -10,14 +10,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class RestFamilyController {
 
